7b9abf39edf9e465459647a56417ef4cf84de5be
VladimirTsvetov <v.a.tsvetov@gmail.com>
2023-03-20 19:11:35 +0300

    add third

---
+++ src/main/java/org/example/ThirdClass.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class ThirdClass {
+    private int aa;
+
+    public int getAa() {
+        return aa;
+    }
+
+    public void setAa(int aa) {
+        this.aa = aa;
+    }
+
+    public ThirdClass(int aa) {
+        this.aa = aa;
+    }
+}
