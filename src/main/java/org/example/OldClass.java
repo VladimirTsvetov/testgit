811591daa16847092276ee43f1f3ad330fdb9a1f
VladimirTsvetov <v.a.tsvetov@gmail.com>
2023-03-20 18:59:24 +0300

    add old class

--- src/main/java/org/example/OldClass.java
+++ src/main/java/org/example/OldClass.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class OldClass {
+    private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
 }
